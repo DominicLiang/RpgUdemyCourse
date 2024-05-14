d5a75cf9f91e5d236110a7a4967c9c2f7872b7db
Dominic Liang <dominic1987@foxmail.com>
2024-05-15 00:01:47 +0800

    test

--- Assets/Scripts/Character/Enemy/Skeleton/Skeleton.cs
+++ Assets/Scripts/Character/Enemy/Skeleton/Skeleton.cs
@@ -5,6 +5,9 @@ public class Skeleton : Character
     protected override void Start()
     {
         base.Start();
+        Debug.Log("hello");
+        Debug.Log("hello");
+        Debug.Log("hello");
     }
 
     protected override void Update()
